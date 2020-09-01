34532b5ad80ed1877dc4928890fe19e887696823
Camila <camila@camis-macbook.local>
2020-08-31 22:56:53 -0300

    add job unifor

---
+++ jobav1.js
@@ -0,0 +1,14 @@
+function contagem(numi,numf){
+    numi = parseInt(numi);
+    numf = parseInt(numf);
+    var contador = 0;
+
+    for (var i = numi ; i < numf ; i++){
+        contador++;
+    }
+    return contador;
+}
+var num1 = 30;
+var num2 = 80;
+var cont = contagem(num1,num2)
+console.log(cont);
